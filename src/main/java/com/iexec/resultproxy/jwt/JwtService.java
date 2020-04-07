b62b26bde5aa359797186bc2848f41c892e22e73
Zied <26070035+zguesmi@users.noreply.github.com>
2020-04-07 14:50:49 +0200

    Move jwt related code to dedicated service

--- src/main/java/com/iexec/resultproxy/jwt/JwtService.java
+++ src/main/java/com/iexec/resultproxy/jwt/JwtService.java
@@ -1,21 +1,62 @@
 package com.iexec.resultproxy.jwt;
 
+import java.util.Date;
 import java.util.Optional;
 
+import com.iexec.resultproxy.auth.SignedChallenge;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Jwts;
 
 @Service
 public class JwtService {
 
-    @Autowired JwtRepository jwtRepository;
+    @Autowired
+    JwtRepository jwtRepository;
+
+    public String getOrCreateJwt(SignedChallenge auth) {
+        String jwtString;
+        Optional<Jwt> oExistingJwt = findByWalletAddress(auth.getWalletAddress());
+
+        if (oExistingJwt.isPresent()) {
+            jwtString = oExistingJwt.get().getJwtString(); // TODO generate new token
+        } else {
+            jwtString = Jwts.builder().setAudience(auth.getWalletAddress()).setIssuedAt(new Date())
+                    .setSubject(RandomStringUtils.randomAlphanumeric(64)).compact();
+            save(new Jwt(auth.getWalletAddress(), jwtString));
+        }
+
+        return jwtString;
+    }
+
+    public boolean isValidJwt(String jwtString) {
+        String claimedWalletAddress = getWalletAddressFromJwtString(jwtString);
+
+        Optional<Jwt> oExistingJwt = findByWalletAddress(claimedWalletAddress);
+
+        if (!oExistingJwt.isPresent()) {
+            return false;
+        }
+
+        if (jwtString.equals(oExistingJwt.get().getJwtString())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String getWalletAddressFromJwtString(String jwtString) {
+        return Jwts.parser().parseClaimsJwt(jwtString).getBody().getAudience();
+    }
 
     public Optional<Jwt> findByWalletAddress(String walletAddress) {
         return jwtRepository.findByWalletAddress(walletAddress);
     }
 
-	public void save(Jwt jwt) {
+    public void save(Jwt jwt) {
         jwtRepository.save(jwt);
-	}
+    }
 }

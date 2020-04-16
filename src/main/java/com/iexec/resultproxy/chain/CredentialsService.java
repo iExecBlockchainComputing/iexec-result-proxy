35accc515b029e39a42beb65e6ddfa4d9d64d78a
Zied <26070035+zguesmi@users.noreply.github.com>
2020-04-16 13:55:07 +0200

    User credentialsService from common

--- src/main/java/com/iexec/resultproxy/chain/CredentialsService.java
+++ src/main/java/com/iexec/resultproxy/chain/CredentialsService.java
@@ -1,34 +1,13 @@
 package com.iexec.resultproxy.chain;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import com.iexec.common.chain.CredentialsAbstractService;
 
 import org.springframework.stereotype.Service;
-import org.web3j.crypto.Credentials;
-import org.web3j.crypto.ECKeyPair;
-import org.web3j.crypto.Keys;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Service
-public class CredentialsService {
+public class CredentialsService extends CredentialsAbstractService {
 
-    private Credentials credentials;
-
-    public CredentialsService() {
-        try {
-            ECKeyPair ecKeyPair = Keys.createEcKeyPair();
-            credentials = Credentials.create(ecKeyPair);
-            log.info("Loaded new wallet credentials [walletAddress:{}] ", credentials.getAddress());
-        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidAlgorithmParameterException e) {
-            log.error("Cannot load wallet credentials");
-            e.printStackTrace();
-        }
-    }
-
-    public Credentials getCredentials() {
-        return credentials;
+    public CredentialsService() throws Exception {
+        super();
     }
 }

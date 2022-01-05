fea81ae3a13c51dd29693762c90034e57c87673b
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-01-05 16:43:54 +0100

    Retrieve version from BuildProperties bean

--- src/main/java/com/iexec/resultproxy/version/VersionService.java
+++ src/main/java/com/iexec/resultproxy/version/VersionService.java
@@ -2,21 +2,22 @@ package com.iexec.resultproxy.version;
 
 import com.iexec.common.utils.VersionUtils;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VersionService {
 
-    @Value("${version}")
-    private String version;
+    @Autowired
+    BuildProperties buildProperties;
 
     public String getVersion() {
-        return version;
+        return buildProperties.getVersion();
     }
 
     public boolean isSnapshot() {
-        return VersionUtils.isSnapshot(version);
+        return VersionUtils.isSnapshot(getVersion());
     }
 
 }

3bd77c1d30de97f02746287c251fdb838a0c69c6
maxence <maxence.cornaton@iex.ec>
2022-01-06 10:12:09 +0100

    Add Swagger URI

--- src/main/java/com/iexec/resultproxy/OpenApiConfig.java
+++ src/main/java/com/iexec/resultproxy/OpenApiConfig.java
@@ -15,6 +15,9 @@ public class OpenApiConfig {
         this.versionService = versionService;
     }
 
+    /*
+     * Swagger URI: /swagger-ui/index.html
+     */
     @Bean
     public OpenAPI api() {
         return new OpenAPI().info(

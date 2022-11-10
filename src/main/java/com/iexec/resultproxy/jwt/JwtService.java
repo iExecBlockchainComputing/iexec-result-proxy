1216da3dbb077d2a62e25658bad61b8e20574d42
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-11-10 11:27:58 +0100

    Sign JWT tokens and add JwtServiceTests

--- src/main/java/com/iexec/resultproxy/jwt/JwtService.java
+++ src/main/java/com/iexec/resultproxy/jwt/JwtService.java
@@ -1,18 +1,39 @@
+/*
+ * Copyright 2022 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.iexec.resultproxy.jwt;
 
 import com.iexec.common.security.SignedChallenge;
 import com.iexec.common.utils.ContextualLockRunner;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.Date;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class JwtService {
+    static final int KEY_SIZE = 128;
+    private static final byte[] jwtKey = SecureRandom.getSeed(KEY_SIZE);
     private final ContextualLockRunner<String> contextualLockRunner = new ContextualLockRunner<>();
-
     private final JwtRepository jwtRepository;
 
     public JwtService(JwtRepository jwtRepository) {
@@ -34,30 +55,45 @@ public class JwtService {
         if (oExistingJwt.isPresent()) {
             jwtString = oExistingJwt.get().getJwtString(); // TODO generate new token
         } else {
-            jwtString = Jwts.builder().setAudience(walletAddress).setIssuedAt(new Date())
-                    .setSubject(RandomStringUtils.randomAlphanumeric(64)).compact();
+            jwtString = createJwt(walletAddress);
             save(new Jwt(walletAddress, jwtString));
         }
         return jwtString;
     }
 
+    String createJwt(String walletAddress) {
+        return Jwts.builder()
+                .setAudience(walletAddress)
+                .setIssuedAt(new Date())
+                .setSubject(RandomStringUtils.randomAlphanumeric(64))
+                .signWith(SignatureAlgorithm.HS256, jwtKey)
+                .compact();
+    }
+
     public boolean isValidJwt(String jwtString) {
-        String claimedWalletAddress = getWalletAddressFromJwtString(jwtString);
-
-        Optional<Jwt> oExistingJwt = findByWalletAddress(claimedWalletAddress);
-
-        return oExistingJwt.isPresent() && jwtString.equals(oExistingJwt.get().getJwtString());
+        try {
+            String claimedWalletAddress = getWalletAddressFromJwtString(jwtString);
+            Jwt existingJwt = findByWalletAddress(claimedWalletAddress).orElseThrow();
+            return jwtString.equals(existingJwt.getJwtString());
+        } catch (Exception e) {
+            log.warn("Invalid JWT token [message:{}]", e.getMessage());
+            return false;
+        }
     }
 
     public String getWalletAddressFromJwtString(String jwtString) {
-        return Jwts.parser().parseClaimsJwt(jwtString).getBody().getAudience();
+        return Jwts.parser()
+                .setSigningKey(jwtKey)
+                .parseClaimsJws(jwtString)
+                .getBody()
+                .getAudience();
     }
 
-    public Optional<Jwt> findByWalletAddress(String walletAddress) {
+    private Optional<Jwt> findByWalletAddress(String walletAddress) {
         return jwtRepository.findByWalletAddress(walletAddress);
     }
 
-    public void save(Jwt jwt) {
+    private void save(Jwt jwt) {
         jwtRepository.save(jwt);
     }
 }
---
+++ src/test/java/com/iexec/resultproxy/jwt/JwtServiceTests.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2022 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iexec.resultproxy.jwt;
+
+import com.iexec.common.security.SignedChallenge;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
+
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.Optional;
+
+import static com.iexec.resultproxy.jwt.JwtService.KEY_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class JwtServiceTests {
+
+    @Mock
+    private JwtRepository jwtRepository;
+    @InjectMocks
+    private JwtService jwtService;
+    private String walletAddress;
+    private static final byte[] badJwtKey = SecureRandom.getSeed(KEY_SIZE);
+
+    private String getWalletAddress() {
+        try {
+            ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+            return Credentials.create(ecKeyPair).getAddress();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        walletAddress = getWalletAddress();
+    }
+
+    //region createJwt and getWalletAddressFromJwtString
+    @Test
+    void readWalletAddressFromValidToken() {
+        String token = jwtService.createJwt(walletAddress);
+        String extractedToken = jwtService.getWalletAddressFromJwtString(token);
+        assertAll(
+                () -> verifyNoInteractions(jwtRepository),
+                () -> assertEquals(walletAddress, extractedToken)
+        );
+    }
+
+    @Test
+    void failToReadWalletAddressOnWronglySignedToken() {
+        String badToken = Jwts.builder()
+                .setAudience(walletAddress)
+                .setIssuedAt(new Date())
+                .setSubject(RandomStringUtils.randomAlphanumeric(64))
+                .signWith(SignatureAlgorithm.HS256, badJwtKey)
+                .compact();
+        assertAll(
+                () -> verifyNoInteractions(jwtRepository),
+                () -> assertThrows(SignatureException.class,
+                        () -> jwtService.getWalletAddressFromJwtString(badToken),
+                        "JWT signature does not match locally computed signature. JWT validity cannot be asserted and should not be trusted.")
+        );
+    }
+
+    @Test
+    void failToReadWalletAddressOnUnsignedToken() {
+        String unsignedToken = Jwts.builder()
+                .setAudience(walletAddress)
+                .setIssuedAt(new Date())
+                .setSubject(RandomStringUtils.randomAlphanumeric(64))
+                .compact();
+        assertAll(
+                () -> verifyNoInteractions(jwtRepository),
+                () -> assertThrows(UnsupportedJwtException.class,
+                        () -> jwtService.getWalletAddressFromJwtString(unsignedToken),
+                "Unsigned Claims JWTs are not supported.")
+        );
+    }
+    //endregion
+
+    //region getOrCreateJwt
+    @Test
+    void createJwtIfNotPresentInRepository() {
+        SignedChallenge signedChallenge = SignedChallenge.builder().walletAddress(walletAddress).build();
+        when(jwtRepository.findByWalletAddress(walletAddress)).thenReturn(Optional.empty());
+        String jwtToken = jwtService.getOrCreateJwt(signedChallenge);
+        assertAll(
+                () -> verify(jwtRepository).findByWalletAddress(walletAddress),
+                () -> verify(jwtRepository).save(any()),
+                () -> assertEquals(walletAddress, jwtService.getWalletAddressFromJwtString(jwtToken))
+        );
+    }
+
+    @Test
+    void getJwtIfPresentInRepository() {
+        String token = jwtService.createJwt(walletAddress);
+        SignedChallenge signedChallenge = SignedChallenge.builder().walletAddress(walletAddress).build();
+        Jwt expectedJwt = new Jwt(walletAddress, token);
+        when(jwtRepository.findByWalletAddress(walletAddress)).thenReturn(Optional.of(expectedJwt));
+        String resultToken = jwtService.getOrCreateJwt(signedChallenge);
+        assertAll(
+                () -> verify(jwtRepository).findByWalletAddress(any()),
+                () -> assertEquals(token, resultToken)
+
+        );
+    }
+    //endregion
+
+    //region isValidJwt
+    @Test
+    void validTokenIsValid() {
+        String token = jwtService.createJwt(walletAddress);
+        when(jwtRepository.findByWalletAddress(walletAddress)).thenReturn(Optional.of(new Jwt(walletAddress, token)));
+        boolean isValid = jwtService.isValidJwt(token);
+        assertAll(
+                () -> verify(jwtRepository).findByWalletAddress(any()),
+                () -> assertTrue(isValid)
+        );
+    }
+
+    @Test
+    void unsignedTokenIsNotValid() {
+        String unsignedToken = Jwts.builder()
+                .setAudience(walletAddress)
+                .setIssuedAt(new Date())
+                .setSubject(RandomStringUtils.randomAlphanumeric(64))
+                .compact();
+        boolean isValid = jwtService.isValidJwt(unsignedToken);
+        assertAll(
+                () -> verifyNoInteractions(jwtRepository),
+                () -> assertFalse(isValid)
+        );
+    }
+
+    @Test
+    void wronglySignedTokenIsNotValid() {
+        String badToken = Jwts.builder()
+                .setAudience(walletAddress)
+                .setIssuedAt(new Date())
+                .setSubject(RandomStringUtils.randomAlphanumeric(64))
+                .signWith(SignatureAlgorithm.HS256, badJwtKey)
+                .compact();
+        boolean isValid = jwtService.isValidJwt(badToken);
+        assertAll (
+                () -> verifyNoInteractions(jwtRepository),
+                () -> assertFalse(isValid)
+        );
+    }
+
+    @Test
+    void tokenIsNotValidWhenNotInRepository() {
+        String token = jwtService.createJwt(walletAddress);
+        when(jwtRepository.findByWalletAddress(walletAddress)).thenReturn(Optional.empty());
+        boolean isValid = jwtService.isValidJwt(token);
+        assertAll(
+                () -> verify(jwtRepository).findByWalletAddress(any()),
+                () -> assertFalse(isValid)
+        );
+    }
+
+    @Test
+    void tokenIsNotValidWhenTamperedRepository() {
+        String forgedToken = jwtService.createJwt(getWalletAddress());
+        when(jwtRepository.findByWalletAddress(walletAddress)).thenReturn(Optional.of(new Jwt(walletAddress, forgedToken)));
+        String queryToken = jwtService.createJwt(walletAddress);
+        boolean isValid = jwtService.isValidJwt(queryToken);
+        assertAll(
+                () -> verify(jwtRepository).findByWalletAddress(any()),
+                () -> assertFalse(isValid)
+        );
+    }
+    //endregion
+
+}

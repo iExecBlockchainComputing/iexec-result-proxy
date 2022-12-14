7908fe62780fdee3f799bb325163decaea879d7a
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-12-14 15:15:54 +0100

    Change annotations order in Jwt to gather them by type (mongo, then lombok)

--- src/main/java/com/iexec/resultproxy/jwt/Jwt.java
+++ src/main/java/com/iexec/resultproxy/jwt/Jwt.java
@@ -24,8 +24,8 @@ import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
 @Document
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Jwt {

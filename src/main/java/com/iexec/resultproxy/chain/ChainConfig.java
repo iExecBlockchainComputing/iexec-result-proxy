4722739b8a07cf85d6106ada2b998553ab375d6c
Jeremy Bernard <jeremy.bernard@iex.ec>
2023-06-07 15:30:57 +0200

    `ChainConfig` instances are immutable

--- src/main/java/com/iexec/resultproxy/chain/ChainConfig.java
+++ src/main/java/com/iexec/resultproxy/chain/ChainConfig.java
@@ -16,22 +16,22 @@
 
 package com.iexec.resultproxy.chain;
 
-import lombok.Data;
+import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 import java.time.Duration;
 
-@Data
+@Value
 @ConstructorBinding
 @ConfigurationProperties(prefix = "chain")
 //TODO: validate configuration property names and use the same set of names everywhere (blockchain-adapter-api, sms)
 public class ChainConfig {
-    private final int id;
-    private final boolean sidechain;
-    private final String privateAddress;
-    private final String hubAddress;
-    private final Duration blockTime;
-    private final float gasPriceMultiplier;
-    private final long gasPriceCap;
+    int id;
+    boolean sidechain;
+    String privateAddress;
+    String hubAddress;
+    Duration blockTime;
+    float gasPriceMultiplier;
+    long gasPriceCap;
 }

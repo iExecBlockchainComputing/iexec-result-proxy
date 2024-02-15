f7a8d3537ccb6bd19880f5f40f298c9ca70b08cf
Jeremy Bernard <jeremy.bernard@iex.ec>
2024-02-15 12:33:45 +0100

    Enable tests in `IpfsResultServiceTest`

--- src/test/java/com/iexec/resultproxy/ipfs/IpfsResultServiceTest.java
+++ src/test/java/com/iexec/resultproxy/ipfs/IpfsResultServiceTest.java
@@ -1,17 +1,36 @@
+/*
+ * Copyright 2020-2024 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.iexec.resultproxy.ipfs;
 
-import com.iexec.resultproxy.chain.IexecHubService;
-
+import com.iexec.commons.poco.utils.BytesUtils;
+import com.iexec.resultproxy.ipfs.task.IpfsNameService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 class IpfsResultServiceTest {
 
     @Mock
-    private IexecHubService iexecHubService;
+    private IpfsNameService ipfsNameService;
 
     @Mock
     private IpfsService ipfsService;
@@ -27,46 +46,43 @@ class IpfsResultServiceTest {
         chainTaskId = "0x1";
     }
 
-    /*
     @Test
     void shouldGetIpfsHashFromChainTaskId() {
         String resultLink = "/ipfs/QmfZ88JXmx2FJsAxT4ZsJBVhBUXdPoRbDZhbkSS1WsMbUA";
-        when(iexecHubService.getTaskResults(chainTaskId, 0)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
+        when(ipfsNameService.getIpfsHashForTask(chainTaskId)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
 
-        assertThat(ipfsResultService.getIpfsHashFromChainTaskId(chainTaskId).equals("QmfZ88JXmx2FJsAxT4ZsJBVhBUXdPoRbDZhbkSS1WsMbUA")).isTrue();
+        assertThat(ipfsResultService.getResult(chainTaskId)).isNotNull();
     }
 
     @Test
     void shouldNotGetIpfsHashFromChainTaskIdSinceNoTaskResult() {
         String resultLink = "/";
-        when(iexecHubService.getTaskResults(chainTaskId, 0)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
+        when(ipfsNameService.getIpfsHashForTask(chainTaskId)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
 
-        assertThat(ipfsResultService.getIpfsHashFromChainTaskId(chainTaskId)).isEmpty();
+        assertThat(ipfsResultService.getResult(chainTaskId)).isEmpty();
     }
 
     @Test
     void shouldNotGetIpfsHashFromChainTaskIdSinceNotHexaString() {
-        when(iexecHubService.getTaskResults(chainTaskId, 0)).thenReturn("0xefg");
+        when(ipfsNameService.getIpfsHashForTask(chainTaskId)).thenReturn("0xefg");
 
-        assertThat(ipfsResultService.getIpfsHashFromChainTaskId(chainTaskId)).isEmpty();
+        assertThat(ipfsResultService.getResult(chainTaskId)).isEmpty();
     }
 
     @Test
     void shouldNotGetIpfsHashFromChainTaskIdSinceNotIpfsLink() {
         String resultLink = "https://customrepo.com/results/abc";
-        when(iexecHubService.getTaskResults(chainTaskId, 0)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
+        when(ipfsNameService.getIpfsHashForTask(chainTaskId)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
 
-        assertThat(ipfsResultService.getIpfsHashFromChainTaskId(chainTaskId)).isEmpty();
+        assertThat(ipfsResultService.getResult(chainTaskId)).isEmpty();
     }
 
     @Test
     void shouldNotGetIpfsHashFromChainTaskIdSinceNotIpfsHash() {
         String resultLink = "/ipfs/ipfs/123";
-        when(iexecHubService.getTaskResults(chainTaskId, 0)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
+        when(ipfsNameService.getIpfsHashForTask(chainTaskId)).thenReturn(BytesUtils.bytesToString(resultLink.getBytes()));
 
-        assertThat(ipfsResultService.getIpfsHashFromChainTaskId(chainTaskId)).isEmpty();
+        assertThat(ipfsResultService.getResult(chainTaskId)).isEmpty();
     }
-
-    */
 
 }
